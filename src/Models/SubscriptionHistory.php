3a2abc68a1b36774e6f809193fb8e53690a793b2
loic.ngou <loic.ngou98@gmail.com>
2024-11-05 13:10:50 +0100

    added subscription_histories table definition

--- src/Models/SubscriptionHistory.php
+++ src/Models/SubscriptionHistory.php
@@ -3,8 +3,15 @@
 namespace NtechServices\SubscriptionSystem\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use NtechServices\SubscriptionSystem\Helpers\ConfigHelper;
 
 class SubscriptionHistory extends Model
 {
     protected $guarded = [];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = ConfigHelper::getConfigTable('subscription_histories','subscription_histories');
+    }
 }
