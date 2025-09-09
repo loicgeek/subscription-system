9d08a43ab9925c851e0ee02e3693611070c2a31d
loic.ngou <loic.ngou98@gmail.com>
2025-09-09 16:56:58 +0100

    added is active

---
+++ src/Database/Migrations/CreateFeaturesTable_02.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace NtechServices\SubscriptionSystem\Database\Migrations;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use NtechServices\SubscriptionSystem\Helpers\ConfigHelper;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table(ConfigHelper::getConfigTable('features'), function (Blueprint $table) {
+            $table->boolean('is_active')->nullable()->default(true);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table(ConfigHelper::getConfigTable('features'), function (Blueprint $table) {
+            $table->dropColumn("is_active");
+           
+        });
+    }
+};
---
+++ src/Database/Migrations/CreatePlansTable_03.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace NtechServices\SubscriptionSystem\Database\Migrations;
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use NtechServices\SubscriptionSystem\Helpers\ConfigHelper;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table(ConfigHelper::getConfigTable('plans'), function (Blueprint $table) {
+            $table->boolean('is_active')->nullable()->default(true);
+           
+        });
+    }
+
+    public function down()
+    {
+        Schema::table(ConfigHelper::getConfigTable('plans'), function (Blueprint $table) {
+            $table->dropColumn('is_active');
+           
+        });
+    }
+};
